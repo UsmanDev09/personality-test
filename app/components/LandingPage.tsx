1cbbc71ca04ce4af2adef7977a855f26d9c731c2
Usman Siddique <usmansiddique@192.168.1.15>
2024-07-10 11:33:20 +0500

    Documented the components

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -14,14 +14,14 @@ export const LandingPage = () => {
                 <h1 className="text-3xl md:text-5xl text-center mb-4">Are you an Introvert or an Extrovert?</h1>
                 <p className="text-center mb-8">Take this quiz to discover your personality type!</p>
                 <Link href='/quiz'>
-                    <p className="bg-green-500 text-white py-4 px-8 rounded hover:bg-green-800">Start Quiz</p>
+                    <button type="button" className="bg-green-500 text-black py-4 px-8 rounded hover:bg-green-800 hover:text-white">Start Quiz</button>
                 </Link>
                 <Link href='questions/new' className="underline text-gray-500">
                     Add questions
                 </Link>
             </div>
             <div className="flex-1 hidden md:flex">
-                <Image src='/side.png' alt="Avatar" className="object-cover w-full h-full" width={400} height={400} />
+                <Image priority src='/side.webp' alt="Avatar" className="object-cover w-full h-full" width={400} height={800} />
             </div>
         </main>
     );
--- app/components/Quiz.tsx
+++ app/components/Quiz.tsx
@@ -6,7 +6,7 @@ import { Result } from "../components/Result";
 import { Question } from "../components/Question";
 import { Choices } from "../components/Choices";
 import { useEffect, useState } from "react";
-import { Choice, Question as QuestionType } from "../types/types";
+import { Choice, Quiz as QuizType } from "../types/types"; // QuizType because the component name is Quiz
 import toast from "react-hot-toast";
 import { Raleway } from "next/font/google";
 
@@ -15,7 +15,13 @@ const raleway = Raleway({
     subsets: ['latin'],
 })
 
-export const Quiz = ({questions}: {questions: QuestionType[]}) => {
+/**
+ * Quiz component for handling quiz navigation, progress and result.
+ * @component
+ * @param {Object} quiz - Information about the quiz comprises id, question and choices
+*/
+
+export const Quiz = ({quiz}: {quiz: QuizType[]}) => {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
     const [submitted, setSubmitted] = useState(false);
@@ -23,10 +29,10 @@ export const Quiz = ({questions}: {questions: QuestionType[]}) => {
     const [selectedChoices, setSelectedChoices] = useState<Choice[]>([]);
 
     useEffect(() => {
-        if(!questions || questions?.length === 0) { 
+        if(!quiz || quiz?.length === 0) { 
             toast.error('No questions available!')
         }
-    }, [questions])
+    }, [quiz])
 
     const onNext = () => {
         setCurrentQuestion(currentQuestion + 1);
@@ -66,14 +72,14 @@ export const Quiz = ({questions}: {questions: QuestionType[]}) => {
                 <Result score={score}/>
             ) : (
                 <>
-                    <ProgressBar progress={currentQuestion / (questions?.length) * 100 ? currentQuestion / (questions?.length) * 100 : 0} />
+                    <ProgressBar progress={currentQuestion / (quiz?.length) * 100 ? currentQuestion / (quiz?.length) * 100 : 0} />
                     <div className="flex flex-col gap-2 mt-10">
-                        <Question question={questions && questions[currentQuestion]?.question} />
-                        <Choices choices={questions && questions[currentQuestion]?.choices} questionNumber={currentQuestion} handleSelectedChoices={handleSelectChoice} selectedChoices={selectedChoices} />
+                        <Question question={quiz && quiz[currentQuestion]?.question} />
+                        <Choices choices={quiz && quiz[currentQuestion]?.choices} questionNumber={currentQuestion} handleSelectedChoices={handleSelectChoice} selectedChoices={selectedChoices} />
                         <div className="flex gap-4 mt-10">
-                            {currentQuestion < (questions?.length) - 1 && <button data-testid='next-button' type="button" disabled={nextBtnDisabled} className={`${nextBtnDisabled ? 'bg-gray-500' : 'bg-green-500 hover:bg-green-800'} text-white p-4 w-28 rounded`} onClick={onNext}>Next</button>}
-                            {currentQuestion === (questions?.length) - 1 && <button data-testid='submit-button' disabled={nextBtnDisabled} className={`${nextBtnDisabled ? 'bg-gray-500' : 'bg-green-500 hover:bg-green-800'} text-white p-4 w-28 rounded`} onClick={() => (selectedChoices[currentQuestion]) ? setSubmitted(true) : setNextBtnDisabled(true)}>Submit</button>}
-                            {currentQuestion > 0 && <button data-testid='back-button' className="bg-green-500 text-white p-4 w-28 rounded hover:bg-green-800" onClick={onBack}>Back</button>}
+                            {currentQuestion < (quiz?.length) - 1 && <button data-testid='next-button' type="button" disabled={nextBtnDisabled} className={`${nextBtnDisabled ? 'bg-gray-500 text-white' : 'bg-green-500 hover:bg-green-800'} text-black p-4 w-28 rounded hover:text-white`} onClick={onNext}>Next</button>}
+                            {currentQuestion === (quiz?.length) - 1 && <button data-testid='submit-button' disabled={nextBtnDisabled} className={`${nextBtnDisabled ? 'bg-gray-500 text-white' : 'bg-green-500 hover:bg-green-800'} text-black p-4 w-28 rounded hover:text-white`} onClick={() => (selectedChoices[currentQuestion]) ? setSubmitted(true) : setNextBtnDisabled(true)}>Submit</button>}
+                            {currentQuestion > 0 && <button data-testid='back-button' className="bg-green-500 text-black p-4 w-28 rounded hover:bg-green-800 hover:text-white" onClick={onBack}>Back</button>}
                         </div>
                     </div>
                 </>
